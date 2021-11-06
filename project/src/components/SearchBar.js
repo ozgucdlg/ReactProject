e8fb00483b64b54f165a22a768cc65877f757e4d
DALGA Ozguc <ozgucdalga@gmail.com>
2021-11-05 23:38:02 -0400

    last update in the current version

--- project/src/components/SearchBar.js
+++ project/src/components/SearchBar.js
@@ -12,21 +12,21 @@ class SearchBar extends React.Component {
             <form onSubmit={this.handleFormSubmit}>
                 <br/>
                 <div className="row" >
-                    <div className="col-10"  >
+                    <section className="col-10"  >
                         <input
                             onChange={this.props.searchMovieProp}
                             type="text" className="form-control"
                             placeholder="Search a movie *"
                         />
-                    </div>
-                    <div className="col-2">
+                    </section>
+                    <section className="col-2">
                         <Link
                             to="/add"
                             type="button"
                             className="btn btn-md btn-danger"
                             style={{ float: 'right' }}> Add Movie
                         </Link>
-                    </div>
+                    </section>
 
                     
                 </div>
