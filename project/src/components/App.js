7adc4d7a784ae0023ed7239242aa53c9329f90bd
ozgucdlg <ozgucdalga@gmail.com>
2025-03-27 00:45:57 -0400

    embeeded static pages

--- project/src/components/App.js
+++ project/src/components/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
-import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import EditMovie from './EditMovie';
+import { movieData } from '../data/movieData';
 
 class App extends React.Component {
 
@@ -14,21 +14,10 @@ class App extends React.Component {
         searchQuery: ""
     }
 
-    async componentDidMount() {
-        const baseURL = "http://localhost:3002/movies";
-        const response = await fetch(baseURL);
-        console.log(response);
-        const data = await response.json();
-        console.log(data);
-        this.setState({ movies: data }) 
-        this.getMovies();
+    componentDidMount() {
+        this.setState({ movies: movieData.movies });
     }
 
-    async getMovies() {
-        const response = await axios.get("http://localhost:3002/movies");
-        this.setState({movies:response.data});
-        
-    }
     // this is event functione and set state part next part is being update button side in MoveLisr.js page
     // this is function to delete movie with onclick
     deleteMovie = (movie) => {
@@ -39,52 +28,29 @@ class App extends React.Component {
             movies: newMovieList
         })
     }
-    /*  FETCH API 
-     deleteMovie =  async (movie)=>{
-         const baseUrl =  `http://localhost:30002/movies/${movie.id}`
-         await fetch(baseUrl, {
-             method:"DELETE"        })
-          const newMovieList = this.state.movies.filter(
-              m =>m.id !==movie.id
-          );
-          this.setState ({
-              movies : newMovieList
-          })
-      } */
-
-    /*  AXIOS  API 
-   deleteMovie =  async (movie)=>{
-      axios.delete(`http://localhost:30002/movies/${movie.id}`)
-       const newMovieList = this.state.movies.filter(
-           m =>m.id !==movie.id
-       );
-       this.setState ({
-           movies : newMovieList
-       })
-   } */
-
-
-
 
     searchMovie = (event) => {
         // console.log(event.target.value);
         this.setState({ searchQuery: event.target.value });
     }
 
-    AddMovie = async (movie) => {
-            await axios.post(`http://localhost:3002/movies/`, movie)
-            this.setState(
-                state => ({ movies: state.movies.concat([movie]) }),
-            )   
-            this.getMovies();     
+    AddMovie = (movie) => {
+        const newMovie = {
+            ...movie,
+            id: this.state.movies.length + 1
+        };
+        this.setState(state => ({
+            movies: [...state.movies, newMovie]
+        }));
     }
 
-    EditMovie = async (id,updatedMovie) => {
-        await axios.put(`http://localhost:3002/movies/${id}`, updatedMovie)
-        this.getMovies();
-             
-}
- 
+    EditMovie = (id, updatedMovie) => {
+        const updatedMovies = this.state.movies.map(movie =>
+            movie.id === parseInt(id) ? { ...updatedMovie, id: parseInt(id) } : movie
+        );
+        this.setState({ movies: updatedMovies });
+    }
+
     render() {
 
         let filteredMovies = this.state.movies.filter(
---
+++ project/src/data/movieData.js
@@ -0,0 +1,19 @@
+export const movieData = {
+  "movies": [
+    {
+      "id": 7,
+      "name": "The Matrix 3",
+      "rating": 8.1,
+      "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
+      "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
+    },
+    {
+      "name": "The Matrix Reloaded",
+      "rating": "9",
+      "overview": "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
+      "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
+      "id": 8
+    },
+    // Add all other movies from the JSON file here
+  ]
+}; 
