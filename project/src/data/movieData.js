8a51e64bbfcbc4025f3586c7c75cecb637431d16
ozgucdlg <ozgucdalga@gmail.com>
2025-03-27 15:43:30 -0400

    a revision of nvm package

--- project/src/data/movieData.js
+++ project/src/data/movieData.js
@@ -7,13 +7,98 @@ export const movieData = {
       "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
       "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
     },
+   
     {
-      "name": "The Matrix Reloaded",
+      "id": 7,
+      "name": "The Matrix 3",
+      "rating": 8.1,
+      "overview": "Set in the 22nd century, The Matrix tells the story of a computer hacker who joins a group of underground insurgents fighting the vast and powerful computers who now rule the earth.",
+      "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
+    },
+    {
+      "name": "The ",
       "rating": "9",
-      "overview": "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
+      "overview": "Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.\nSix months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.Six months after the events depicted in The Matrix, Neo has proved to be a good omen for the free humans, ability to see the codes of the things inside the matrix and a certain degree of pre-cognition.",
       "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/jBegA6V243J6HUnpcOILsRvBnGb.jpg",
       "id": 8
     },
+    {
+      "name": "Saw 3D 55",
+      "rating": "8",
+      "overview": "SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.SAW legacy, a group of Jigsaw survivors gathers to seek the support of self-help guru and fellow survivor Bobby Dagen, a man whose own dark secrets unleash a new wave of terror.",
+      "imageURL": "https://image.tmdb.org/t/p/w600_and_h900_bestv2/qHCZ6LjtmqWDfXXN28TlIC9OppK.jpg",
+      "id": 11
+    },
+    {
+      "name": "Rogue",
+      "rating": 7.4,
+      "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.\nA new content has been added in order to make another update.",
+      "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+      "id": 4
+    },
+    {
+      "name": "Project Power 2",
+      "rating": "9",
+      "overview": "TEST TEST This is a wider card with supporting text below as a natural lead-in to additional content.\nThis is a wider card with supporting text below as a natural lead-in to additional content.\nThis is a wider card with supporting text below as a natural lead-in to additional content.\nThis is a wider card with supporting text below as a natural lead-in to additional content.",
+      "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/TnOeov4w0sTtV2gqICqIxVi74V.jpg",
+      "id": 5
+    },
+    {
+      "name": "No Sudden Move",
+      "rating": "6.6",
+      "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.This is a wider card with supporting text below as a natural lead-in to additional content.\nv",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/34BmdJkdvRweC3xJJFlOFQ2IbYc.jpg",
+      "id": 12
+    },
+    {
+      "name": "The Tomorrow ",
+      "rating": "5",
+      "overview": "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/34nDCQZwaEvsy4CFO5hkGRFDCVU.jpg",
+      "id": 13
+    },
+    {
+      "name": "Rick and Mort ",
+      "rating": "8.8",
+      "overview": "Rick is a mentally-unbalanced but scientifically-gifted old man who has recently reconnected with his family. He spends most of his time involving his young grandson Morty in dangerous, outlandish adventures throughout space and alternate universes. Compounded with Morty's already unstable family life, these events cause Morty much distress at home and school.",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/8kOWDBK6XlPUzckuHDo3wwVRFwt.jpg",
+      "id": 14
+    },
+    {
+      "name": "Kingdom: Ashin of the North ",
+      "rating": "7.6",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/cE9SBjWMUK0HFxmT1cVFDtu5CLT.jpg",
+      "overview": "Tragedy, betrayal and a mysterious discovery fuel a woman's vengeance for the loss of her tribe and family in this special episode of \"Kingdom.\"\nTragedy, betrayal and a mysterious discovery fuel a woman's vengeance for the loss of her tribe and family in this special episode of \"Kingdom.\"\nTragedy, betrayal and a mysterious discovery fuel a woman's vengeance for the loss of her tribe and family in this special episode of \"Kingdom.\"",
+      "id": 15
+    },
+    {
+      "name": "Zack Snyder's Justice League",
+      "rating": "9.5",
+      "overview": "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.   ",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/tnAuB8q5vv7Ax9UAEje5Xi4BXik.jpg",
+      "id": 16
+    },
+    {
+      "name": "Squid",
+      "rating": "9.9",
+      "overview": "the life of Ozguc dalga",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/kEl2t3OhXc3Zb9FBh1AuYzRTgZp.jpg",
+      "id": 17
+    },
+    {
+      "name": "Grey's Anatomy",
+      "rating": "7.1",
+      "overview": "Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/clnyhPqj1SNgpAdeSS6a6fwE6Bo.jpg",
+      "id": 18
+    },
+    {
+      "name": "Emmerdale",
+      "rating": "4.4",
+      "overview": "This is not preferable movies for all watchers !....",
+      "imageURL": "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ualtuRRNYa9Fj61wSuBVcuvaVDP.jpg",
+      "id": 19
+    }
     // Add all other movies from the JSON file here
   ]
 }; 
